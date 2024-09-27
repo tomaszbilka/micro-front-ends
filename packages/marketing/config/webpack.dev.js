5356dfb30bbae4fe6d0bccf0f15d9ccfe1a26126
tomaszbilka <bila4@op.pl>
2024-09-27 08:08:21 +0200

    initial commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,30 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packadeJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap.js",
+      },
+      shared: packadeJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
